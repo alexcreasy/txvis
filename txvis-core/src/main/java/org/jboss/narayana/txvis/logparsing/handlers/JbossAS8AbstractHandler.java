f3c5ce599fa177d336bff4457020c6d3cf79db50
Alex Creasy <arcreasy@gmail.com>
2013-06-28 22:16:18 +0100

    Fixed bug with date parser

--- txvis-core/src/main/java/org/jboss/narayana/txvis/logparsing/handlers/JbossAS8AbstractHandler.java
+++ txvis-core/src/main/java/org/jboss/narayana/txvis/logparsing/handlers/JbossAS8AbstractHandler.java
@@ -3,8 +3,10 @@ package org.jboss.narayana.txvis.logparsing.handlers;
 import org.jboss.narayana.txvis.logparsing.AbstractHandler;
 
 import java.sql.Timestamp;
+import java.text.MessageFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * @Author Alex Creasy &lt;a.r.creasy@newcastle.ac.uk$gt;
@@ -83,16 +85,34 @@ public abstract class JbossAS8AbstractHandler extends AbstractHandler {
     /**
      * Parses a String time value in <code>HH:mm:ss,SSS</code> format.
      *
-     * @param time a String containing the time to parse.
+     * @param dateTime a String containing the time to parse.
      * @return a <code>java.sql.Timestamp</code> representation of the given time.
      * @throws NullPointerException if time is null.
      * @throws IllegalArgumentException if time is not in the format <code>HH:mm:ss,SSS</code>
      */
-    protected final Timestamp parseTimestamp(String time) throws NullPointerException, IllegalArgumentException {
+    protected final Timestamp parseTimestamp(String dateTime) {
+        int hour = -1;
+        int minute = -1;
+        int second = -1;
+        int millis = -1;
+
         try {
-            return new Timestamp(new SimpleDateFormat("HH:mm:ss,SSS").parse(time).getTime());
-        } catch (ParseException e) {
-            throw new IllegalArgumentException("Illegal time format, must be HH:mm:ss,SSS");
+            hour = Integer.parseInt(dateTime.substring(0, 2));
+            minute = Integer.parseInt(dateTime.substring(3, 5));
+            second = Integer.parseInt(dateTime.substring(6, 8));
+            millis = Integer.parseInt(dateTime.substring(9));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(MessageFormat.format(
+                    "Utils.parseTimeStamp could not parse: {0}  / tokens: hour={1}, minute={2}, second={3}, millis={4}",
+                    dateTime, hour, minute, second, millis), e);
         }
+
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, hour);
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, second);
+        c.set(Calendar.MILLISECOND, millis);
+
+        return new Timestamp(c.getTimeInMillis());
     }
 }
